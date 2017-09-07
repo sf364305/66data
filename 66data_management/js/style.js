d44053409757f883720cfa27ae1a745223b44430
石凡 <1839085395@qq.com>
2017-09-07 10:09:12 +0800

    66数据管家

---
+++ 66data_management/js/style.js
@@ -0,0 +1,67 @@
+/* 
+* @Author: anchen
+* @Date:   2017-05-02 11:42:50
+* @Last Modified by:   anchen
+* @Last Modified time: 2017-09-06 15:10:36
+*/
+
+$(document).ready(function(){
+    // 左侧栏的高设置,右侧宽设置
+    var windowH = $(window).height() - 78;
+    var windowW = $(window).width()- 230;
+    $(".index-left").css({
+        "height": windowH
+    });
+    $(".index-wrap, .detail-wrap").css({
+        "width": windowW
+    });
+    $(".index-left a").mouseover(function(event) {
+        $(this).css({
+            "color": "#fff",
+            "text-decoration": "none"
+        })
+    });
+    $(".guild-add-n").hover(function() {
+        $(this).children("ul").css({
+            "display": "block"
+        })
+        var liH = $(this).children("ul").height()-25;
+        $(this).children("ul").css({
+            "bottom": -liH,
+            "left": "40px"
+        })
+    }, function() {
+        $(this).children("ul").css({
+            "display": "none"
+        })
+    });
+    $(".add-list-guild li").click(function() {
+        var guildT = $(this).html();
+        $(".guild-name-s").html(guildT);
+        $(this).parent().css("display","none")
+    })
+    $(".nav-slide").click(function() {
+        var navH = $(this).height()+20;
+        var liH = $(this).parent().height();
+        console.log(navH);
+        console.log(liH);
+        if(liH > navH) {
+            $(this).siblings('.nav-inner').css({
+                "display": "none"
+            })
+        } else {
+            console.log("555")
+            $(this).siblings('.nav-inner').css({
+                "display": "block"
+            })
+        }
+    })
+    $(".btn").click(function(event) {
+        $(this).css({
+            "color": "#1cb1cf",
+            "border": "none",
+            "outline": "none",
+            "background": "none"
+        })
+    });
+});
