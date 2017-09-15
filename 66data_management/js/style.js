afacb03563146979500bc63ad546511f78ae64ef
石凡 <1839085395@qq.com>
2017-09-15 10:20:59 +0800

    重写alert

--- 66data_management/js/style.js
+++ 66data_management/js/style.js
@@ -2,7 +2,7 @@
 * @Author: anchen
 * @Date:   2017-05-02 11:42:50
 * @Last Modified by:   anchen
-* @Last Modified time: 2017-09-14 18:46:56
+* @Last Modified time: 2017-09-15 10:16:22
 */
 
 $(document).ready(function(){
@@ -64,36 +64,107 @@ $(document).ready(function(){
             $(".mloading").css("display","none")
         },1000)
     });
-    // 提示框调用
-       $("#alertBtn").click(function(){
-            
-           $.DialogByZ.Alert({Title: "提示", Content: "您的请求失败",BtnL:"确定",FunL:function(){$.DialogByZ.Close();}}) 
-       })
-       window.alert = function(){
-             $("body").css({
-                 "overflow-y": "hidden"
-             })
-            $.DialogByZ.Alert({Title: "提示", Content: "您的请求失败",BtnL:"确定",FunL:alerts})
+       //弹出确认框
+       var showAlert = function(msg){
+           jAlert(msg, "提示！");
+       }
+       window.alert=showAlert;
+       $.alerts={
+           verticalOffset:-75,horizontalOffset:0,repositionOnResize:true,overlayOpacity:.5,overlayColor:'rgb(100,100,100)',draggable:true,okButton:' 确定 ',cancelButton:' Cancel ',dialogClass:null,alert:function(message,title,callback){
+               if(title==null)title='Alert';
+               $.alerts._show(title,message,null,'alert',function(result){
+                   if(callback)callback(result)
+               })
+           }
+           ,_show:function(title,msg,value,type,callback){
+               $.alerts._hide();
+               $.alerts._overlay('show');
+               $("BODY").append('<div id="popup_container">'+'<h1 id="popup_title"></h1>'+'<div id="popup_content">'+'<div id="popup_message"></div>'+'</div>'+'</div>');
+               if($.alerts.dialogClass)$("#popup_container").addClass($.alerts.dialogClass);
+               var pos=($.browser.msie&&parseInt($.browser.version)<=6)?'absolute':'fixed';
+               $("#popup_container").css({
+                   position:pos,zIndex:99999,padding:0,margin:0
+               });
+               $("#popup_title").text(title);
+               $("#popup_content").addClass(type);
+               $("#popup_message").text(msg);
+               $("#popup_message").html($("#popup_message").text().replace(/\n/g,'<br />'));
+               $("#popup_container").css({
+                   minWidth:$("#popup_container").outerWidth(),maxWidth:$("#popup_container").outerWidth()
+               });
+               $.alerts._reposition();
+               $.alerts._maintainPosition(true);
+               switch(type){
+                   case'alert':$("#popup_message").after('<div id="popup_panel"><input type="button" value="'+$.alerts.okButton+'" id="popup_ok" /></div>');
+                   $("#popup_ok").click(function(){
+                       $.alerts._hide();
+                       callback(true)
+                   });
+                   $("#popup_ok").focus().keypress(function(e){
+                       if(e.keyCode==13||e.keyCode==27)$("#popup_ok").trigger('click')
+                   });
+                   break
+               };
+               if($.alerts.draggable){
+                   try{
+                       $("#popup_container").draggable({
+                           handle:$("#popup_title")
+                       });
+                       $("#popup_title").css({
+                           cursor:'move'
+                       })
+                   }
+                   catch(e){}
+               }
+           }
+           ,_hide:function(){
+                $("body").css({
+                    "overflow-y": "auto"
+                });
+               $("#popup_container").remove();
+               $.alerts._overlay('hide');
+               $.alerts._maintainPosition(false)
+           }
+           ,_overlay:function(status){
+               switch(status){
+                   case'show':$.alerts._overlay('hide');
+                    $("body").css({
+                           "overflow-y": "hidden"
+                    });
+                   $("BODY").append('<div id="popup_overlay"></div>');
+                   $("#popup_overlay").css({
+                       position:'absolute',zIndex:99998,top:'0px',left:'0px',width:'100%',height:$(document).height(),background:$.alerts.overlayColor,opacity:$.alerts.overlayOpacity
+                   });
+                   break;
+                   case'hide':$("#popup_overlay").remove();
+                   break
+               }
+           }
+           ,_reposition:function(){
+               var top=(($(window).height()/ 2) - ($("#popup_container").outerHeight() /2))+$.alerts.verticalOffset;
+               var left=(($(window).width()/ 2) - ($("#popup_container").outerWidth() /2))+$.alerts.horizontalOffset;
+               if(top<0)top=0;
+               if(left<0)left=0;
+               if($.browser.msie&&parseInt($.browser.version)<=6)top=top+$(window).scrollTop();
+               $("#popup_container").css({
+                   top:top+'px',left:left+'px'
+               });
+               $("#popup_overlay").height($(document).height())
+           }
+           ,_maintainPosition:function(status){
+               if($.alerts.repositionOnResize){
+                   switch(status){
+                       case true:$(window).bind('resize',function(){
+                           $.alerts._reposition()
+                       });
+                       break;
+                       case false:$(window).unbind('resize');
+                       break
+                   }
+               }
+           }
        };
-       $("#confirmBtn").click(function(){
-           $.DialogByZ.Confirm({Title: "", Content: "投资前需开通第三方存管账户</br>确保资金安全",FunL:confirmL,FunR:Immediate})
-       })
-       $("#toastBtn").click(function(){
-          $.DialogByZ.Autofade({Content: "感谢您对我们的支持"})
-       })
-       $("#load").click(function(){
-          $.DialogByZ.Loading('image/loading.png') 
-       })
-    ///
-       // function confirmL(){
-       //          $.DialogByZ.Close();
-       //          $.DialogByZ.Alert({Title: "提示", Content: "您要求稍后开通",BtnL:"确定"})
-       // }
-       //
-       // function alerts(){
-       //      $("body").css({
-       //          "overflow-y": "auto"
-       //      })
-       //      $.DialogByZ.Close();
-       // }
+       jAlert=function(message,title,callback){
+           $.alerts.alert(message,title,callback)
+       }
 });
